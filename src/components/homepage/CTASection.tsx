6ab4c4c6944a8623e4691e68945e6837b5c6611a
Ethan Diedericks <51838346+ethandiedericks@users.noreply.github.com>
2025-06-01 03:38:27 +0200

    corrected mobile cta aspect ratio

--- src/components/homepage/CTASection.tsx
+++ src/components/homepage/CTASection.tsx
@@ -24,8 +24,8 @@ export const CTASection = () => {
               <Image
                 src={CTA}
                 alt="Nevaeh Solutions Call To Action"
-                width={1440}
-                height={713}
+                width={710}
+                height={560}
               />
             </div>
             <div className=" flex justify-center items-center">
